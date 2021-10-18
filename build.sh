d9a727197ae49b0b9d3ab24121f5ad493a90c512
Zababurin <s.zababurin.v@gmail.com>
2021-10-18 21:44:04 +0300

    add build.sh for ES6 module

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+make  -f Makefile.PHONY.wasi clean
+make  distclean
+FLAGS="
+-Os \
+-s USE_SDL_GFX=2 \
+-s USE_ZLIB=1 \
+-s USE_SDL=2 \
+-s USE_SDL_MIXER=2 \
+--preload-file data/  \
+-s ASYNCIFY=1  \
+-s ALLOW_MEMORY_GROWTH=1 \
+-s USE_ES6_IMPORT_META=0 \
+-s -s ASSERTIONS=1 \
+-s USE_ZLIB=1 \
+-s MODULARIZE=1 \
+-s EXPORT_ES6=1 \
+-s INVOKE_RUN=1"
+
+sudo docker run --rm -v $(pwd):/src -u $(id -u):$(id -g) emscripten/emsdk \
+emmake make  \
+LDFLAGS_native="$FLAGS" \
+CC_native=emcc  \
+EXE_native=index.mjs \
+CFLAGS_native="$FLAGS"
